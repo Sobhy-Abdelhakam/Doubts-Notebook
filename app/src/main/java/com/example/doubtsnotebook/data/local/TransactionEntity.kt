79b2a7cd78f10847840dc78dae75f51381f26892
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-07-22 14:37:26 +0300

    add database entities

---
+++ app/src/main/java/com/example/doubtsnotebook/data/local/TransactionEntity.kt
@@ -0,0 +1,27 @@
+package com.example.doubtsnotebook.data.local
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "transactions",
+    foreignKeys = [
+        ForeignKey(
+            entity = CustomerEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["customerId"],
+            onDelete = CASCADE,
+        )
+    ]
+)
+data class TransactionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val customerId: Int,
+    val type: String, // "PURCHASE" or "PAYMENT"
+    val description: String?,
+    val amount: Double,
+    val date: Long = System.currentTimeMillis()
+)
