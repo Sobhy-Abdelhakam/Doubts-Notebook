7caccc54155d6f551f0ba3960421037971fc59ef
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-07-23 11:16:50 +0300

    refactor customer view model

--- app/src/main/java/com/example/doubtsnotebook/presentation/customers/CustomerViewModel.kt
+++ app/src/main/java/com/example/doubtsnotebook/presentation/customers/CustomerViewModel.kt
@@ -2,29 +2,16 @@ package com.example.doubtsnotebook.presentation.customers
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.doubtsnotebook.domain.model.Customer
 import com.example.doubtsnotebook.domain.repository.CustomerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class CustomerViewModel @Inject constructor(private val repository: CustomerRepository): ViewModel() {
-    private val _customers = MutableStateFlow<List<Customer>>(emptyList())
-    val customers = _customers.asStateFlow()
+class CustomerViewModel @Inject constructor(private val repository: CustomerRepository) :
+    ViewModel() {
 
-    init {
-        getAllCustomers()
-    }
-
-    private fun getAllCustomers() {
-        viewModelScope.launch {
-            repository.getAllCustomers()
-                .collect { customerList ->
-                    _customers.value = customerList
-                }
-        }
-    }
+    val customers = repository.getAllCustomers()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
 }
