94d0e7f2aab6b1605cb7e99744b6f05f9120df7c
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-07-23 11:36:46 +0300

    implement search in customer list

--- app/src/main/java/com/example/doubtsnotebook/presentation/customers/CustomerListScreen.kt
+++ app/src/main/java/com/example/doubtsnotebook/presentation/customers/CustomerListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
@@ -14,12 +15,19 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -32,6 +40,7 @@ fun CustomerListScreen(
     onAddCustomerClick: () -> Unit,
 ) {
     val customers by viewModel.customers.collectAsState()
+    var searchQuery by remember { mutableStateOf("") }
 
     Scaffold(
         topBar = { TopAppBar(title = { Text("notebook") }) },
@@ -55,8 +64,25 @@ fun CustomerListScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(padding)
+                    .padding(16.dp)
             ) {
-                items(customers) { customer ->
+                item {
+                    OutlinedTextField(
+                        value = searchQuery,
+                        onValueChange = { searchQuery = it },
+                        modifier = Modifier.fillMaxWidth().padding(8.dp),
+                        placeholder = {
+                            Text("Search")
+                        },
+                        shape = RoundedCornerShape(16.dp)
+                        )
+                }
+                items(
+                    customers.filter {
+                        it.name.contains(searchQuery, ignoreCase = true)
+                    },
+                    key = { it.id }
+                ) { customer ->
                     Card(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -64,7 +90,7 @@ fun CustomerListScreen(
                         elevation = CardDefaults.cardElevation(4.dp)
                     ) {
                         Column(modifier = Modifier.padding(16.dp)) {
-                            Text(customer.name)
+                            Text(customer.name, style = MaterialTheme.typography.titleMedium)
                             customer.phone?.let { phone ->
                                 Text(phone)
                             }
