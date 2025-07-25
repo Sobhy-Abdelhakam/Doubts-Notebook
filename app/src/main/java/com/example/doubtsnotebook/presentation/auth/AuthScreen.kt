c9d463f8665a4232deffb8d7ba297902af1e4d8d
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-07-25 16:55:48 +0300

    add auth screen

---
+++ app/src/main/java/com/example/doubtsnotebook/presentation/auth/AuthScreen.kt
@@ -0,0 +1,122 @@
+package com.example.doubtsnotebook.presentation.auth
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.doubtsnotebook.R
+
+@Composable
+fun AuthScreen(
+    viewModel: AuthViewModel = hiltViewModel(),
+    onAuthSuccess: () -> Unit
+) {
+    val authState = viewModel.authState
+    var selectedTab by remember { mutableIntStateOf(0) }
+
+    val tabs = listOf(stringResource(R.string.login), stringResource(R.string.register))
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TabRow(selectedTabIndex = selectedTab) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        selected = selectedTab == index,
+                        onClick = { selectedTab = index },
+                        text = { Text(title) }
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(stringResource(R.string.email)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            var isPasswordVisible by remember { mutableStateOf(false) }
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(stringResource(R.string.password)) },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(
+                        onClick = {
+                            isPasswordVisible = !isPasswordVisible
+                        }
+                    ) {
+                        Icon(
+                            painterResource(if (isPasswordVisible) R.drawable.visible else R.drawable.not_visible),
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = {
+                    if (selectedTab == 0) {
+                        viewModel.login(email, password)
+                    } else {
+                        viewModel.register(email, password)
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(if (selectedTab == 0) stringResource(R.string.login) else stringResource(R.string.register))
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            when (authState) {
+                is AuthState.Loading -> CircularProgressIndicator()
+                is AuthState.Error -> Text(authState.message, color = Color.Red)
+                is AuthState.Success -> {
+                    LaunchedEffect(Unit) { onAuthSuccess() }
+                }
+
+                else -> {}
+            }
+        }
+    }
+}
